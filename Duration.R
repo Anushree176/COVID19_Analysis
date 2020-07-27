8af140ceb8cbb0909740fc34246750b3c01331e4
Anushree176 <anushree.kolhe18@siesgst.ac.in>
2020-07-27 18:33:23 +0530

    Code added.

---
+++ Duration.R
@@ -0,0 +1,14 @@
+indi <- as.data.frame(read.csv("IndividualDetails.csv"))
+indi <- subset(indi,select = -c(1,2,4,5,6,7,9,12))
+indi$diagnosed_date <- as.Date(indi$diagnosed_date, format = "%d/%m/%Y")
+indi$status_change_date <- as.Date(indi$status_change_date, format = "%d/%m/%Y")
+duration <- round(abs(indi$status_change_date-indi$diagnosed_date), 0)
+indi <- cbind(indi, duration)
+
+indi = indi[!indi$duration < 6,]
+
+indi <- indi[complete.cases(indi), ]
+rownames(indi) <- c(1:149)
+mean(indi$duration)
+plot_ly(indi, y = ~duration, type = "scatter")
+
