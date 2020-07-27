38fb294fa175f072b7a7730b3f380c2bd0e7f5dd
Anushree176 <anushree.kolhe18@siesgst.ac.in>
2020-07-27 22:28:32 +0530

    Updated

--- Num&Types_labs.R
+++
@@ -1,8 +0,0 @@
-labs <- as.data.frame(read.csv("ICMRTestingLabs.csv", header = TRUE))
-labs <- labs[,5:6]
-typeof(labs)
-labs <- labs[with(labs, order(labs$state)), ]
-labs_present <- cbind(labs[-112,])
-labs_present_plt <- plot_ly(labs_present, x = ~state, y = ~type)
-labs_present_plt <- labs_present_plt %>% layout(title = "Number & types of Labs in each state")
-labs_present_plt
